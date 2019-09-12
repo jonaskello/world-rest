57f23d6dce1c415b13e936c1b4b1b6193683474a
Jonas Kello <jonas.kello@gmail.com>
2019-09-12 15:26:35 +0200

    Simplify

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,27 @@
 import express from "express";
-import {
-  createConnectionPool,
-  getCities,
-  getCity,
-  getCountries,
-  getCountry
-} from "./db";
 import cors from "cors";
+import * as db from "./db";
 
 require("source-map-support").install();
 
 async function startServer(): Promise<void> {
-  const pool = await createConnectionPool();
-
+  const pool = await db.createConnectionPool();
   const app = express();
   app.use(cors());
-  app.get("/cities", async (_req, res) => {
-    res.send(await getCities(pool));
-  });
-  app.get("/cities/:id", async (req, res) => {
-    res.send(await getCity(pool, parseInt(req.params.id)));
-  });
   app.get("/countries", async (_req, res) => {
-    res.send(await getCountries(pool));
+    res.send(await db.getCountries(pool));
   });
   app.get("/countries/:code", async (req, res) => {
-    res.send(await getCountry(pool, req.params.code));
+    res.send(await db.getCountry(pool, req.params.code));
   });
-
-  const port = 4000;
-  const ip = "0.0.0.0";
-  app.listen(port, ip);
-  console.log(`Server listening at http://${ip}:${port}`);
+  app.get("/cities", async (_req, res) => {
+    res.send(await db.getCities(pool));
+  });
+  app.get("/cities/:id", async (req, res) => {
+    res.send(await db.getCity(pool, parseInt(req.params.id)));
+  });
+  app.listen(4000, "0.0.0.0");
+  console.log(`Server listening at http://"0.0.0.0":4000`);
 }
 
 startServer();
